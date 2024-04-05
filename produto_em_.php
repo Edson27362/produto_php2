af54ce90a96202dfddabfbcd5bd8f781e1c52b6d
Edson27362 <127742066+Edson27362@users.noreply.github.com>
2024-04-05 11:32:35 -0300

    Add files via upload

---
+++ produto_em_.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Exibe e calcula o preço de venda do produto</title>
+</head>
+<body>
+    <?php
+    echo "<h3> EXIBE E CALCULA O PREÇO DE VENDA DO PRODUTO</h3>";
+    echo "<h3>";
+    // Recupera os dados informados nas caixas de texto e armazena nas variáveis
+    $codigo = $_POST["codigo"];
+    $descricao = $_POST["descricao"];
+    $marca  = $_POST["marca"];
+    $PrecoCusto  = $_POST["PrecoCusto"];
+    $PercAumento  = $_POST["PercentualAumento"];
+
+    echo "Código: " .$codigo;
+    echo "<br>Descrição: " .$descricao;
+    echo "<br>Marca: " .$marca;
+    echo "<br>Preço de Custo: " .$PrecoCusto;
+    echo  "<br>% de aumento: " .$PercAumento . "%";
+    echo "<hr>";
+    echo "<marquee behavior='alternate'>Desenvolvido por Otávio Chera Gato - &copy; 2024</ marquee";
+    ?>
+</body>
+</html>
